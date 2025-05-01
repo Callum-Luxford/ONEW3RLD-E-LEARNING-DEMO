5fb3d756e0115aa41eab3d32659c6e0292ef71f6
Callum Luxford <151152271+Callum-Luxford@users.noreply.github.com>
2025-05-01 11:05:43 +0100

    Slight change to guestMiddleware

--- server/middleware/guestMiddleware.js
+++ server/middleware/guestMiddleware.js
@@ -5,13 +5,13 @@ const jwt = require("jsonwebtoken");
 const redirectIfAuthenticated = (req, res, next) => {
   const token = req.cookies.token;
 
-  if (!token) return next(); // not logged in → allow access
+  if (!token) return next(); // not logged in -> allow access
 
   try {
     jwt.verify(token, process.env.JWT_SECRET);
-    return res.redirect("/dashboard"); // token valid → redirect
+    return res.redirect("/dashboard"); // token valid -> redirect
   } catch (err) {
-    return next(); // token invalid → continue to login/register
+    return next(); // token invalid -> continue to login/register
   }
 };
 
